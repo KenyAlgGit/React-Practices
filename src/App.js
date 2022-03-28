ae29b02481ee75653ddd76d4b1353f4a35d204b1
KenyAlgGit <ao000010@algonquinlive.com>
2022-03-28 16:37:09 -0400

    done

--- src/App.js
+++ src/App.js
@@ -2,36 +2,28 @@
 import './App.css';
 import restaurant from "./restaurant.jpg"
 
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useReducer} from "react"
 
 function App() {
-  const what = useState("Happy")//use Stage created a array, here it's the 'what'
-  console.log(what)
 
-  const [emotion, setEmotion] = useState("happy")
-  //here 'emotion' means the first element of this array, now is 'happy'
-  //this means, the default value is 'happy'
+  // const [checked, setChecked] = useState(false)
+  // //default value of 'checked' is false
 
-  useEffect( () => {
-    //watch out the sentence here to use variable in a log
-    console.log(`It's ${emotion} around here!`, [emotion])
-  })
+  // function toggle(){
+  //   setChecked( (checked) => !checked )
+  //   //change "checked" value to true
+  // }
+
+  const [checked, toggle] = useReducer( (checked) => !checked, false )
+  //default value of 'checked' is false
 
   return (
     <>
-      <h1>Current emotion is {emotion}</h1>
-
-      <button onClick = { () => setEmotion("happy") }>
-        Happy
-      </button>
-
-      <button onClick = { () => setEmotion("frustrated") }>
-        Frustrated
-      </button>
-
-      <button onClick = { () => setEmotion("ethusiastic") }>
-        Ethuse
-      </button>
+      <input type='checkbox' 
+      value={checked}
+      onChange={ toggle }
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </>
   )
 }
