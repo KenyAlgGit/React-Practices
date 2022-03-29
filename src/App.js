712d07b0d0a1935942674795d7e58b41c55e14da
KenyAlgGit <ao000010@algonquinlive.com>
2022-03-28 22:31:30 -0400

    done

--- src/App.js
+++ src/App.js
@@ -18,14 +18,27 @@ function App({login}) {
 
   const [data, setData] = useState(null)
   //default value is null
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
+    //if login fail then skip this block
+    if(!login) return;
+    //otherwise trun loading into true
+    setLoading(true)
     fetch(`https://api.github.com/users/${login}`)
     .then((response) => response.json())
     .then(setData)
-  }, [])
+    .then(() => setLoading(false))
+    .catch(setError)
+  }, [login])
 
-  if(data){
+  if(loading) return <h1>Loading</h1>
+  if(error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>
+  }
+  if(!data) {return null}
+  else{
     return (
       <div>
         <h1>Name: {data.name}</h1>
@@ -35,13 +48,6 @@ function App({login}) {
       </div>
     )
   }
-  else{
-    return <div>No user Available</div>
-  }
-
-  return (
-    <div></div>
-  )
 }
 
 export default App;
